c03491f090fed2746dbb2a38aa19b9fa8e9c6c26
ltsPapo <156462256+ltsPapo@users.noreply.github.com>
2025-04-04 16:14:03 +0000

    Changed cardscreen to homescreen, add viewsavedroutines and addroutine

---
+++ flutter_app/lib/screens/AddRoutineScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AddRoutineScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Routine')),
+      body: Center(child: Text('Form to add a new routine.')),
+    );
+  }
+}
