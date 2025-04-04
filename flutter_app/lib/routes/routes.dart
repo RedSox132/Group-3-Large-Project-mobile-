172208268fc8ac02eef12284caa3886e3161760a
ltsPapo <156462256+ltsPapo@users.noreply.github.com>
2025-04-04 16:18:52 +0000

    Changed routes for CardScreen to HomeScreen, added routes for AddRoutine and ViewSavedRoutines
    Since forgotpassword isn't functional yet on the web app, I've set it to where it reroutes any old cards routes to homescreen. still need to fix forgotpassword route.

--- flutter_app/lib/routes/routes.dart
+++ flutter_app/lib/routes/routes.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart'; // Import Flutter's material design package
 import 'package:flutter_app/screens/LoginScreen.dart'; // Import the LoginScreen widget
 import 'package:flutter_app/screens/RegisterScreen.dart';  // Import the RegisterScreen widget
-import 'package:flutter_app/screens/CardsScreen.dart'; // Import the CardsScreen widget
+import 'package:flutter_app/screens/HomeScreen.dart'; // Import the HomeScreen widget
+import 'package:flutter_app/screens/ViewSavedRoutines.dart'; //Import the ViewSavedRoutines widget
+import 'package:flutter_app/screens/AddRoutineScreen.dart'; //Import the AddRoutine widget
 
 
 class Routes {
   // Define route names as constants
   static const String LOGINSCREEN = '/login';
   static const String REGISTER_SCREEN = '/register';  
-  static const String CARDSSCREEN = '/cards';
+  static const String HOMESCREEN = '/home';
   static const String FORGOT_PASSWORD_SCREEN = '/cards';
+  static const String VIEW_ROUTINES_SCREEN = '/view-routines';
+  static const String ADD_ROUTINE_SCREEN = '/add-routine';
 
   // Define a map of routes
   static Map<String, Widget Function(BuildContext)> get getroutes => {
@@ -23,6 +27,15 @@ class Routes {
         REGISTER_SCREEN: (context) => RegisterScreen(), 
         
         // Cards screen route
-        CARDSSCREEN: (context) => CardsScreen(),
+        HOMESCREEN: (context) => HomeScreen(),
+
+        //ViewRoutines screen route
+        VIEW_ROUTINES_SCREEN: (context) => ViewSavedRoutines(),
+
+        //AddRoutines screen route
+        ADD_ROUTINE_SCREEN: (context) => AddRoutineScreen(),
+
+        //auto reroutes any old cards routes to homescreen (tempfix)
+        '/cards': (context) => HomeScreen(),
       };
 }
